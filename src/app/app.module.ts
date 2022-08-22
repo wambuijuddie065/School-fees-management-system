d9e846e8ab3a61002ebb285f06af7a8e17e3b7d2
Judy Wambui <wambuijuddie065@gmail.com>
2022-08-22 20:02:55 +0300

    added page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,13 +8,17 @@ import { HeroComponent } from './hero/hero.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 
 const appRouter:Routes=[
-  {path:'',component:HeroComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'login',component:LoginComponent}
+  // {path:'', component: HeroComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home', component: HeroComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'login', component: LoginComponent},
+  {path:'**',component:PageNotFoundComponent}
 
 ]
 
@@ -23,13 +28,14 @@ const appRouter:Routes=[
     HeroComponent,
     LoginComponent,
     DashboardComponent,
+    PageNotFoundComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
